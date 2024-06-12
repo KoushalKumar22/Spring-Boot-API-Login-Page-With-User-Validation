5bbae8b72b3bb1354e007950896e3a06dba975cc
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-13 00:22:31 +0530

    first commit

---
+++ Login-page/src/test/java/com/SpringAPI/Login_page/LoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAPI.Login_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
