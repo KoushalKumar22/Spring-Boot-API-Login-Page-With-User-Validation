73be4e40a43d1784f260da4bdd1ff617a2bc890d
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-13 00:39:41 +0530

    Jpa Repository connected

--- Login-page/src/main/java/com/SpringAPI/Login_page/Repository/LoginPageRepository.java
+++ Login-page/src/main/java/com/SpringAPI/Login_page/Repository/LoginPageRepository.java
@@ -1,4 +1,10 @@
 package com.SpringAPI.Login_page.Repository;
 
-public interface LoginPageRepository {
+import com.SpringAPI.Login_page.Entity.LoginPage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoginPageRepository extends JpaRepository<LoginPage,String> {
+    Optional<LoginPage> findByUsername(String username);
 }
